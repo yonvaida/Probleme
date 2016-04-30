6910b80cd9fddd3422a85b241891be8dd01ef613
Vaida Ionut <ionut.vaida@gmail.com>
2016-04-30 21:19:05 +0300

    pentru primele 26 de coloane

--- Coloane Excel/Coloane Excel/excelColumns.cs
+++ Coloane Excel/Coloane Excel/excelColumns.cs
@@ -12,13 +12,31 @@ namespace Coloane_Excel
             Assert.AreEqual("A", excelColumnName(1));
         }
         [TestMethod]
+        public void mColumnTest()
+        {
+            Assert.AreEqual("M", excelColumnName(13));
+        }
+        [TestMethod]
         public void aaColumnTest()
         {
-            Assert.AreEqual("AA", excelColumnName(1));
+            Assert.AreEqual("AA", excelColumnName(27));
+        }
+        [TestMethod]
+        public void azColumnTest()
+        {
+            Assert.AreEqual("AZ", excelColumnName(52));
         }
     string excelColumnName(int number)
         {
-            string[] columnName = { "","A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" };
+            string[] columnChar = { "","A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" };
+
+            if (number < 26)
+            {
+                return columnChar[number];
+            }else
+            {
+                return "";
+            }
             
         }
     }
