d66423b4d01ac9dd2083895e0927f57b91b0e236
Vaida Ionut <ionut.vaida@gmail.com>
2016-04-30 20:53:54 +0300

    creare teste si stringul cu literele alfabetului

--- Coloane Excel/Coloane Excel/excelColumns.cs
+++ Coloane Excel/Coloane Excel/excelColumns.cs
@@ -7,9 +7,19 @@ namespace Coloane_Excel
     public class excelColumns
     {
         [TestMethod]
-        public void TestMethod1()
+        public void firstColumnTest()
         {
             Assert.AreEqual("A", excelColumnName(1));
+        }
+        [TestMethod]
+        public void aaColumnTest()
+        {
+            Assert.AreEqual("AA", excelColumnName(1));
+        }
+    string excelColumnName(int number)
+        {
+            string[] columnName = { "","A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" };
+            
         }
     }
 }
