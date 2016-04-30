0d3ded385fb490ec63832a6f5ff29c40a6c791c8
Vaida Ionut <ionut.vaida@gmail.com>
2016-04-30 20:31:09 +0300

    Initial

---
+++ Coloane Excel/Coloane Excel/excelColumns.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Coloane_Excel
+{
+    [TestClass]
+    public class excelColumns
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("A", excelColumnName(1));
+        }
+    }
+}
