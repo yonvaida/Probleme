a81b8d5fb935bf248abbbd0d08edb5febe210994
Vaida Ionut <ionut.vaida@gmail.com>
2016-05-03 16:43:09 +0300

    Varianta rezolvata variante loto

--- Loto/Loto/loto.cs
+++ Loto/Loto/loto.cs
@@ -7,19 +7,49 @@ namespace Loto
     public class loto
     {
         [TestMethod]
+        public void MergesN7K3()
+        {
+            Assert.AreEqual(35, Merges(7,3));
+        }
+        [TestMethod]
+        public void MergesN7K0()
+        {
+            Assert.AreEqual(1, Merges(7, 0));
+        }
+        [TestMethod]
+        public void MergesN49K6()
+        {
+            Assert.AreEqual(13983816, Merges(49, 6));
+        }
+        [TestMethod]
         public void ChanceCatI()
         {
-            Assert.AreEqual(0.00000007, LotoProbability(6));
+            Assert.AreEqual(1/13983816, LotoProbability(6));
         }
         [TestMethod]
         public void ChanceCatII()
         {
-            Assert.AreEqual(0.00001845, LotoProbability(5));
+            Assert.AreEqual(1/54201, LotoProbability(5));
         }
         [TestMethod]
         public void ChanceCatIII()
         {
-            Assert.AreEqual(0.00096862, LotoProbability(4));
+            Assert.AreEqual(1/1032, LotoProbability(4));
         }
+        int Merges(int n,int k)
+        {
+            if (k == 0||n==k)
+            {
+                return 1;
+            }else
+            {
+                return (Merges(n - 1, k - 1) + Merges(n - 1, k));
+            }
+        }
+        decimal LotoProbability(int numbers)
+        {
+            return Merges(6, numbers) * Merges(43, 6 - numbers) / Merges(49, 6);
+        }
+
     }
 }
