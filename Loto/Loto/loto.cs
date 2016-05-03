4eb7831eddaebaf064f1cac206c6d9408c9c149e
Vaida Ionut <ionut.vaida@gmail.com>
2016-05-03 14:45:44 +0300

    Teste loto

--- Loto/Loto/loto.cs
+++ Loto/Loto/loto.cs
@@ -7,9 +7,19 @@ namespace Loto
     public class loto
     {
         [TestMethod]
-        public void probabilitate6Din49()
+        public void ChanceCatI()
         {
-            Assert.AreEqual()
+            Assert.AreEqual(0.00000007, LotoProbability(6));
+        }
+        [TestMethod]
+        public void ChanceCatII()
+        {
+            Assert.AreEqual(0.00001845, LotoProbability(5));
+        }
+        [TestMethod]
+        public void ChanceCatIII()
+        {
+            Assert.AreEqual(0.00096862, LotoProbability(4));
         }
     }
 }
