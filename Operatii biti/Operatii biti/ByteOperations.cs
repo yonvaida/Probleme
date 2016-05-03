908d3398c4e121e6b12d4f488403ff88cbf26bab
Vaida Ionut <ionut.vaida@gmail.com>
2016-05-03 20:23:16 +0300

    initial

---
+++ Operatii biti/Operatii biti/ByteOperations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Operatii_biti
+{
+    [TestClass]
+    public class ByteOperations
+    {
+        [TestMethod]
+        public void NumberToBase2Test()
+        {
+            Assert.AreEqual({1,1,0,0},ConvertTo2(12))
+        }
+    }
+}
