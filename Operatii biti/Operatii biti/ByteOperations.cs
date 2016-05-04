68750ea337fe07680024da47a4afcc9951eca531
yonvaida <yon_vaida@yahoo.com>
2016-05-04 09:10:51 +0300

    Operatia AND implementata

--- Operatii biti/Operatii biti/ByteOperations.cs
+++ Operatii biti/Operatii biti/ByteOperations.cs
@@ -6,23 +6,30 @@ namespace Operatii_biti
     [TestClass]
     public class ByteOperations
     {
+        //******************Convert to byte TEST******************************
         [TestMethod]
         public void NumberToBase2Test()
         {
-            byte[] result = { 0, 0, 1, 1 };
+            byte[] result = { 1, 1, 0, 0 };
             CollectionAssert.AreEqual(result, ConvertTo2(12));
         }
+        //*********************NOT operation TEST**********************************
         [TestMethod]
-        public void NotOperationTest()
+        public void NOTOperationTest()
         {
-            byte[] result = { 1, 1, 0, 0 };
-            CollectionAssert.AreEqual(result, NotOperation(ConvertTo2(12)));
+            byte[] result = { 0, 0, 1, 1 };
+            CollectionAssert.AreEqual(result, NOTOperation(ConvertTo2(12)));
         }
+        //***********************AND operation TEST*********************************
         [TestMethod]
-       
+        public void ANDOperationTest()
+        {
+            byte[] result = { 1, 0, 0, 0 };
+            CollectionAssert.AreEqual(result, ANDOperation(ConvertTo2(12), ConvertTo2(10)));
+        }
 
-
-        byte[] ConvertTo2(int number)
+        //******************Convert to byte******************************
+       byte[] ConvertTo2(int number)
         {
             byte[] result=new byte[1];
             int i=0;
@@ -35,17 +42,34 @@ namespace Operatii_biti
                 number = number / 2;
                 i++;
             } while (number > 0);
+            Array.Reverse(result);
             return result;
         }
-        byte[] NotOperation(byte[] result)
+
+        //*********************NOT operation**********************************
+        byte[] NOTOperation(byte[] result)
         {
-            int length = result.Length;
-            for(int i=0; i < length; i++)
+            for(int i=0; i < result.Length; i++)
             {
                 if (result[i] == 0) result[i] = 1;
                 else result[i] = 0;
             }
             return result;
         }
-    }
+
+        //***********************AND operation*********************************
+        byte[] ANDOperation(byte[] result,byte[] addedlist)
+        {
+            if (result.Length > addedlist.Length) Array.Resize(ref addedlist, result.Length);
+            else Array.Resize(ref result, addedlist.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (result[i] == 1 && addedlist[i] == 1) result[i] = 1;
+                else result[i] = 0;
+            }
+                return result;
+
+        }
+
+   }
 }
