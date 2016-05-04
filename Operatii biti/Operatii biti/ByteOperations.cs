e53741d78c11cd4228badd2c77c6cd883ed9d1e9
yonvaida <yon_vaida@yahoo.com>
2016-05-04 08:51:26 +0300

    Convertirea in baza 2 si implementarea operatiei not

--- Operatii biti/Operatii biti/ByteOperations.cs
+++ Operatii biti/Operatii biti/ByteOperations.cs
@@ -9,7 +9,43 @@ namespace Operatii_biti
         [TestMethod]
         public void NumberToBase2Test()
         {
-            Assert.AreEqual({1,1,0,0},ConvertTo2(12))
+            byte[] result = { 0, 0, 1, 1 };
+            CollectionAssert.AreEqual(result, ConvertTo2(12));
+        }
+        [TestMethod]
+        public void NotOperationTest()
+        {
+            byte[] result = { 1, 1, 0, 0 };
+            CollectionAssert.AreEqual(result, NotOperation(ConvertTo2(12)));
+        }
+        [TestMethod]
+       
+
+
+        byte[] ConvertTo2(int number)
+        {
+            byte[] result=new byte[1];
+            int i=0;
+            do
+            {
+                Array.Resize(ref result,i+1);
+                int tempbyte;
+                tempbyte = number % 2;
+                result[i] = (byte)tempbyte;
+                number = number / 2;
+                i++;
+            } while (number > 0);
+            return result;
+        }
+        byte[] NotOperation(byte[] result)
+        {
+            int length = result.Length;
+            for(int i=0; i < length; i++)
+            {
+                if (result[i] == 0) result[i] = 1;
+                else result[i] = 0;
+            }
+            return result;
         }
     }
 }
