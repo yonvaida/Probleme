32ba53ad94856a30421115956a0672e9971af8d8
yonvaida <yon_vaida@yahoo.com>
2016-05-04 13:50:42 +0300

    ADD Operation

--- Operatii biti/Operatii biti/ByteOperations.cs
+++ Operatii biti/Operatii biti/ByteOperations.cs
@@ -13,6 +13,13 @@ namespace Operatii_biti
             byte[] result = { 1, 1, 0, 0 };
             CollectionAssert.AreEqual(result, ConvertTo2(12));
         }
+        //******************Convert to decimal TEST******************************
+        [TestMethod]
+        public void ConvertTo10Test()
+        {
+            byte[] result = { 1, 1, 0, 0 };
+            Assert.AreEqual(12, ConvertTo10(result));
+        }
         //*********************NOT operation TEST**********************************
         [TestMethod]
         public void NOTOperationTest()
@@ -39,8 +46,6 @@ namespace Operatii_biti
             byte[] result = { 0, 1, 0, 0 };
             CollectionAssert.AreEqual(result, ANDOperation(ConvertTo2(12), ConvertTo2(4)));
         }
-
-
         //***********************OR operation TEST*********************************
         [TestMethod]
         public void OROperationTest()
@@ -55,8 +60,40 @@ namespace Operatii_biti
             byte[] result = { 0, 1, 1, 0 };
             CollectionAssert.AreEqual(result, XOROperation(ConvertTo2(12), ConvertTo2(10)));
         }
-
-
+        //***********************LeftShift operation TEST*********************************
+        [TestMethod]
+        public void LeftShiftOperationTest()
+        {
+            byte[] result = { 1, 0, 0, 0 };
+            CollectionAssert.AreEqual(result, LeftShiftOperation(ConvertTo2(12), 1));
+        }
+        //***********************RightShift operation TEST*********************************
+        [TestMethod]
+        public void RightShiftOperationTest()
+        {
+            byte[] result = { 0, 0, 1, 1 };
+            CollectionAssert.AreEqual(result, RightShiftOperation(ConvertTo2(12), 2));
+        }
+        //***********************Lessthan operation TEST*********************************
+        [TestMethod]
+        public void LessThanOperationTest()
+        {
+           
+        }
+        //***********************ADD operation TEST*********************************
+        [TestMethod]
+        public void ADDOperationTest()
+        {
+            
+            Assert.AreEqual(142, ConvertTo10(ADDOperation(ConvertTo2(127), ConvertTo2(15))));
+        }
+        //***********************Subtract operation TEST*********************************
+        [TestMethod]
+        public void SubtractOperationTest()
+        {
+            byte[] result = { 1, 1, 1, 0, 1 };
+            CollectionAssert.AreEqual(result, SubtractOperation(ConvertTo2(142), ConvertTo2(113)));
+        }
 
         //******************Convert to byte******************************
         byte[] ConvertTo2(int number)
@@ -75,6 +112,18 @@ namespace Operatii_biti
             Array.Reverse(result);
             return result;
         }
+        //******************Convert to decimal******************************
+        double ConvertTo10(byte[] result)
+        {
+            Array.Reverse(result);
+            double number=0;
+            for (int i = 0; i < result.Length; i++)
+            {
+                number = number + Math.Pow(2, i)*(int)result[i];
+            }
+                return number;
+        }
+
 
         //*********************NOT operation**********************************
         byte[] NOTOperation(byte[] result)
@@ -157,5 +206,69 @@ namespace Operatii_biti
 
         }
 
+        //***********************LeftShift operation*********************************
+        byte[] LeftShiftOperation(byte[] result,int iteration)
+        {
+            byte[] resultTemp = result;
+            Array.Resize(ref resultTemp, result.Length + iteration);
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = resultTemp[i + iteration];
+            }
+                return result;
+        }
+
+        //***********************RightShift operation*********************************
+        byte[] RightShiftOperation(byte[] result, int iteration)
+        {
+            Array.Reverse(result);
+            result = LeftShiftOperation(result,iteration);
+            Array.Reverse(result);
+            return result;
+        }
+
+        //***********************LessThan operation***********************************
+
+        //***********************ADD operation****************************************
+        byte[] ADDOperation(byte[] number1,byte[] number2)
+        {
+
+            Array.Reverse(number1);
+            Array.Reverse(number2);
+            if (number1.Length > number2.Length) {
+                Array.Resize(ref number1, number1.Length + 1);
+                Array.Resize(ref number2, number1.Length);
+            }else {
+                Array.Resize(ref number1, number2.Length + 1);
+                Array.Resize(ref number2, number2.Length + 1);
+            };
+            byte transp = 0;
+            for (int i = 0; i < number1.Length; i++)
+            {
+                int sumtemp = (int)number1[i] + (int)number2[i] + (int)transp;
+                number1[i] = (byte)(sumtemp % 2);
+                transp = (byte)(sumtemp / 2);
+            }
+            Array.Reverse(number1);
+            return number1;
+        }
+
+        //***********************Subtract operation*********************************
+        byte[] SubtractOperation(byte[] number1, byte[] number2)
+        {
+            double resulttemp = ConvertTo10(number1) - ConvertTo10(number2);
+            int result = (int)resulttemp;
+            return ConvertTo2(result);
+        }
+
+        //***********************Multiplication operation*********************************
+
+        //***********************Division operation*********************************
+
+        //***********************GratherThan operation*********************************
+
+        //***********************Equal operation*********************************
+
+        //***********************NotEqual operation*********************************
     }
 }
