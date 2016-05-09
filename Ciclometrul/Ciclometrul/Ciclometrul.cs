e775a52bcc63a9c0ed526c53639035d9538fd000
yonvaida <yon_vaida@yahoo.com>
2016-05-09 13:42:28 +0300

    Distanta mai multi concurenti

--- Ciclometrul/Ciclometrul/Ciclometrul.cs
+++ Ciclometrul/Ciclometrul/Ciclometrul.cs
@@ -9,21 +9,32 @@ namespace Ciclometrul
         [TestMethod]
         public void DistanceTest()
         {
-            CompetitorStruct contender = new CompetitorStruct { rotations = 10, lungimeRoata = 2, startNumber=1,Time=30 }; 
-            Assert.AreEqual(600, Distance(contender));
+            CompetitorStruct[] contender = new CompetitorStruct[2] {
+                new CompetitorStruct { rotations = 10, lengthRoata = 2, name = "Popescu", startNumber = 1, totalTime = 300 },
+                new CompetitorStruct { rotations = 8, lengthRoata = 2, name = "Ionescu", startNumber = 2, totalTime = 280 }
+            };
+
+            int[] result = { 6000, 4480 };
+           CollectionAssert.AreEqual(result, Distance(contender));
         }
 
         struct CompetitorStruct{
         public int rotations;
         public int startNumber;
-        public int Time;
-        public int lungimeRoata;
+        public string name;
+        public int totalTime;
+        public int lengthRoata;
         
          }
 
-        int Distance(CompetitorStruct Competitor)
+        int[] Distance(CompetitorStruct[] Competitor)
     {
-       return Competitor.lungimeRoata*Competitor.rotations*Competitor.Time;
+            var result = new int[2];
+            for (int i = 0; i < 2; i++)
+            {
+                result[i] = Competitor[i].lengthRoata * Competitor[i].rotations * Competitor[i].totalTime;
+            }
+            return result;
     }
 
     }
