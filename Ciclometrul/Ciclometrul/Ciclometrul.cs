28285a322645f29a13952cd45c74051a85311da3
yonvaida <yon_vaida@yahoo.com>
2016-05-09 12:57:57 +0300

    Distanta parcursa

--- Ciclometrul/Ciclometrul/Ciclometrul.cs
+++ Ciclometrul/Ciclometrul/Ciclometrul.cs
@@ -7,8 +7,24 @@ namespace Ciclometrul
     public class Ciclometrul
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DistanceTest()
         {
+            CompetitorStruct contender = new CompetitorStruct { rotations = 10, lungimeRoata = 2, startNumber=1,Time=30 }; 
+            Assert.AreEqual(600, Distance(contender));
         }
+
+        struct CompetitorStruct{
+        public int rotations;
+        public int startNumber;
+        public int Time;
+        public int lungimeRoata;
+        
+         }
+
+        int Distance(CompetitorStruct Competitor)
+    {
+       return Competitor.lungimeRoata*Competitor.rotations*Competitor.Time;
+    }
+
     }
 }
