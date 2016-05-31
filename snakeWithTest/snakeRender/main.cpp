d9cd5c868dd53eb35671118ab44f6cf57b547590
ionut vaida <ionut.vaida@gmail.com>
2016-05-31 19:03:50 +0300

    compiled snakwWhitTest as lib and included in snake render

--- snakeWithTest/snakeRender/main.cpp
+++ snakeWithTest/snakeRender/main.cpp
@@ -4,22 +4,26 @@
 #include <qgraphicsview.h>
 #include <QGraphicsRectItem>
 #include <memory>
-#include <boost\property_tree\ptree.hpp>
+//#include <boost\property_tree\ptree.hpp>
 #include "snakeWithTest\table.h"
 
 int main(int argc, char *argv[])
 {
+	table board(500,500);
 	QApplication a(argc, argv);
-	
 	std::unique_ptr<QGraphicsRectItem> boardGui(new QGraphicsRectItem());
 	std::unique_ptr<QGraphicsScene> scene(new QGraphicsScene());
 	std::unique_ptr<QGraphicsView> view(new QGraphicsView(scene.get()));
 	boost::property_tree::ptree data;
+	board.getData(data);
+	view->setFixedSize(data.get<int>("table.width") + 10, data.get<int>("table.height") + 10);
+	boardGui->setRect(0, 0, data.get<int>("table.width"),data.get<int>("table.height"));
 	
-	boardGui->setRect(0, 0, 20,10);
 	scene->addItem(boardGui.get());
 
-	view->show();
 
+
+
+	view->show();
 	return a.exec();
 }
--- snakeWithTest/snakeWithTest/main.cpp
+++ snakeWithTest/snakeWithTest/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	
 	int result = Catch::Session().run(argc, argv);
-
+	
 	
 
 
