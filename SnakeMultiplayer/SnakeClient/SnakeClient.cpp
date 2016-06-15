b770ce8aa1b5a750d8e371686bbc25da345c34fe
yonvaida <yon_vaida@yahoo.com>
2016-06-15 12:32:45 +0300

    send json over the network

--- SnakeMultiplayer/SnakeClient/SnakeClient.cpp
+++ SnakeMultiplayer/SnakeClient/SnakeClient.cpp
@@ -5,10 +5,12 @@
 #include "boost\asio.hpp"
 #include "boost\array.hpp"
 #include <iostream>
+#include <vector>
+#include <string.h>
+#include <boost\property_tree\ptree.hpp>
+#include <boost\property_tree\json_parser.hpp>
 
 int main()
-
-
 {
 	boost::asio::io_service io_service;
 	boost::asio::ip::tcp::resolver resolver(io_service);
@@ -17,17 +19,11 @@ int main()
 	boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
 	boost::asio::ip::tcp::socket socket(io_service);
 	boost::asio::connect(socket,endpoint_iterator);
-	boost::array<char, 128> buf;
-	
-		size_t len = socket.read_some(boost::asio::buffer(buf));
+	std::vector<char> buf(128);
+		socket.read_some(boost::asio::buffer(buf));
+			std::cout<<buf.data() << std::endl;
 		
-			std::cout << buf.data() << std::endl;
-		
-	}
-	
-	
-
-	
+	}	
 	return 0;
 }
 
--- SnakeMultiplayer/SnakeServer/SnakeServer.cpp
+++ SnakeMultiplayer/SnakeServer/SnakeServer.cpp
@@ -3,23 +3,36 @@
 
 #include "stdafx.h"
 #include "boost\asio.hpp"
+#include <boost\property_tree\ptree.hpp>
+#include <boost\property_tree\json_parser.hpp>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
+#include <stdlib.h>
+#include <iostream>
 
 int main()
 {
-	srand(time(NULL));
+	std::ostringstream result;
+	boost::property_tree::ptree data;
+	data.put("val1", "value 1");
+	data.put("val2", "value 2");
+	std::cout << data.get<std::string>("val1") << std::endl;
+	boost::property_tree::write_json(result, data);
+	//std::cout << result.str() << std::endl;
 	
 	boost::asio::io_service io_service;
 	boost::asio::ip::tcp::acceptor acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(),1200));
+	//std::string buffer_content="Server sent info";
 	
+
 	for (;;) {
-		int i = rand();
-		std::string message = std::to_string(i);
+		system("cls");
+		std::cout << "Server is running ..." << std::endl;
 		boost::asio::ip::tcp::socket socket(io_service);
 		acceptor.accept(socket);
-		boost::asio::write(socket, boost::asio::buffer(message), boost::asio::transfer_all());
+		boost::asio::write(socket, boost::asio::buffer(result.str()), boost::asio::transfer_all());
 	}
 
 
