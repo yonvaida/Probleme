4cacda0a6c3ea0599c08c07948f9b9f1d4e127af
yonvaida <yon_vaida@yahoo.com>
2016-08-03 14:54:22 +0300

    webserver read write messages from javascript

--- SnakeMultiplayer/snakeWebServer/snakeWebServer.cpp
+++ SnakeMultiplayer/snakeWebServer/snakeWebServer.cpp
@@ -1,62 +1,24 @@
-// snakeWebServer.cpp : Defines the entry point for the console application.
-//
-#include "boost\asio.hpp"
 #include <iostream>
-#include <vector>
-#include <string>
-int i = 0;
-std::vector<char> buf;
 
-void readwrite(boost::asio::ip::tcp::socket &socket) {
-	buf.resize(1024);
-	boost::asio::async_read(socket, boost::asio::buffer(buf), [&](const boost::system::error_code ec, size_t length) {
-		std::cout << buf.data() << std::endl;
-		boost::asio::async_write(socket, boost::asio::buffer("<html>MERGE</html>"), [&](const boost::system::error_code ec, size_t length) {
-			readwrite(socket);
+#include <websocketpp\config\asio_no_tls.hpp>
+#include <websocketpp\server.hpp>
 
-		});
-
-	});
-
-
-	std::cout << buf.data() << std::endl;
-}
-
-
-void acceptConnection(boost::asio::ip::tcp::socket &serverSocket, boost::asio::ip::tcp::endpoint &serverEndpoint,boost::asio::ip::tcp::acceptor &serverAcceptor) {
-	serverAcceptor.async_accept(serverSocket, serverEndpoint, [&](const boost::system::error_code &ec) {
-		i++;
-		std::vector<char> buf;
-		buf.resize(1);
-		std::cout << "Connection accepted" << std::endl;
-		
-		readwrite(serverSocket);
-		
-	});
-}
-
-
-int main()
-{
-	try {
-		boost::asio::io_service ioService;
-		boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), 1080);
-		boost::asio::ip::tcp::socket serverSocket(ioService);
-		boost::asio::ip::tcp::acceptor serverAcceptor(ioService, endpoint);
-
-		acceptConnection(serverSocket, endpoint, serverAcceptor);
-
-
-
-
-
-
-		ioService.run();
-	}
-	catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
+typedef websocketpp::server<websocketpp::config::asio> server;
+server print_server;
+void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
+	std::cout << msg->get_payload() << std::endl;
+	print_server.send(hdl,"mesaj nou", msg->get_opcode());
 	
-	return 0;
 }
 
+int main() {
+	
+
+	print_server.set_message_handler(&on_message);
+
+	print_server.init_asio();
+	print_server.listen(1080);
+	print_server.start_accept();
+	
+	print_server.run();
+}
